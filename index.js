acbbe4b3fa42df49d2d904a62b44194d8ee9f334
sami suliman <sami.bco@gmail.com>
2019-11-30 14:19:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function writeCards(cards)
 {
    for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you, ${cards[i]}  , for the wonderful birthday gift`);
+   }
+ 
+  return cards;
 }
 
 
