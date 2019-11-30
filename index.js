514555cef51466e6735d6ec730b267f09ff6cd1e
sami suliman <sami.bco@gmail.com>
2019-11-30 14:16:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cards = ["Ada", "Brendan", "Ali"];
 
 function writeCards(cards)
 {
-  
+   for (let i = 0; i < cards.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
 }
 
 
